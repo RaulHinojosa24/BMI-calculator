1c007416e53ce4ebfb268aab761274bf3764094d
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-16 12:51:11 +0100

    Solucionado

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Implementar el código aqui
-
 // Escuchar el evento 'submit'
+
+// const imc = (weight / ((height*height)/10000))
+
+const bmi = (weight, height) => (weight / ((height * height) / 10000));
+
+const form = document.forms["bmi"];
+
+form.addEventListener("submit", calculateBMI);
+
+function calculateBMI(event) {
+    event.preventDefault();
+
+    const height = form.elements["height"].value;
+    const weight = form.elements["weight"].value;
+
+    document.querySelector("#results").textContent = Math.floor(bmi(weight, height) * 100) / 100;
+};
