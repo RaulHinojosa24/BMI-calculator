3760fde64baeddca71aed4d0f914ceca540c695a
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-16 13:21:51 +0100

    2.0

--- script.js
+++ script.js
@@ -15,5 +15,28 @@ function calculateBMI(event) {
     const height = form.elements["height"].value;
     const weight = form.elements["weight"].value;
 
-    document.querySelector("#results").textContent = Math.floor(bmi(weight, height) * 100) / 100;
+    const bmiResult = Math.floor(bmi(weight, height) * 100) / 100;
+
+    document.querySelector("#results").textContent = bmiResult;
+
+    addColorToBMI(bmiResult);
+};
+
+function addColorToBMI(bmiResult) {
+    let color;
+    let text;
+
+    if (bmiResult < 18.6) {
+        color = "firebrick";
+        text = " (under)";
+    } else if (bmiResult <= 24.9) {
+        color = "green";
+        text = " (normal)";
+    } else {
+        color = "firebrick";
+        text = " (over)";
+    }
+
+    document.querySelector("#results").style.color = color;
+    document.querySelector("#results").textContent += text;
 };
